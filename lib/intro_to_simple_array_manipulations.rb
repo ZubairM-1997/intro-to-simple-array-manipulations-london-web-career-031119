5ae674572b5893b1fbb26d656129308fb0556502
placid-computer-9089 <zubairmaqsood866@gmail.com>
2019-02-22 20:18:32 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,8 @@
+cars = ["volkswagen", "BMW", "Toyota", "Mercedes", "Ford"]
+next_car = "Aston Martin"
+cars.push(next_car)
+
+phone_companies = ["Samsung", "Apple","Microsoft", "Sony", "OnePlus" ]
+phone_companies.pop 
+
+phone_companies
