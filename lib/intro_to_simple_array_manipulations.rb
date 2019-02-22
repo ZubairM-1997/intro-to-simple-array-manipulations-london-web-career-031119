6a79460de86d80b61c707aea1fccf80b52a587d8
placid-computer-9089 <zubairmaqsood866@gmail.com>
2019-02-22 20:36:50 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -4,17 +4,17 @@ end
 
 def using_unshift(array, string)
   array.unshift(string)
-  array.size
+ 
 end 
 
 def using_pop(array)
   array.pop 
-  array.size
+  
 end 
 
 def using_shift(array)
   array.shift
-  array.size 
+   
 end 
 
 def pop_with_args(array)
