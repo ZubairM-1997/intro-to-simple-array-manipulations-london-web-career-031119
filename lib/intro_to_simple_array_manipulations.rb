e276b00902c58a4ba5892015c0257acd092c3666
placid-computer-9089 <zubairmaqsood866@gmail.com>
2019-02-22 20:19:14 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,6 +1,12 @@
+#Adding element to end of array
+
 cars = ["volkswagen", "BMW", "Toyota", "Mercedes", "Ford"]
 next_car = "Aston Martin"
 cars.push(next_car)
+
+
+
+#removing element from end of array
 
 phone_companies = ["Samsung", "Apple","Microsoft", "Sony", "OnePlus" ]
 phone_companies.pop 
